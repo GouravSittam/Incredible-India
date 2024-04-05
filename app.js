f88f6acfb9e5507781fc6a5a4f0522d9dedeeffe
Gourav Chaudhary <gouravsittam@gmail.com>
2024-04-06 01:00:34 +0530

    up

--- app.js
+++ app.js
@@ -1,93 +1,94 @@
-const express=require('express');
-const app=express();
-const mongoose=require("mongoose");
-const Listing=require("./models/listing.js");
-const path=require("path");
-const methodOverride=require("method-override");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/IncredibleIndia";
+const MONGO_URL = "mongodb://127.0.0.1:27017/IncredibleIndia";
 
-main().then(()=>{
-    console.log("Connected to DB");
-}).catch((err)=>{
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-
-//Listings the data EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method")); 
+app.use(methodOverride("_method"));
 
-
-
-app.get("/",(req,res)=>{
-    res.send("<h1>Hello World</h1>");
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
 });
 
-//Index route
+//Index Route
 app.get("/listings", async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
-  });
-
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
 
 //New Route
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-  });
- 
+  res.render("listings/new.ejs");
+});
 
-  //Show Route
-  app.get("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", { listing });
-  });
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
 
 //Create Route
 app.post("/listings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  });
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
 
 //Edit Route
 app.get("/listings/:id/edit", async (req, res) => {
-    let { id } = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-  });
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
 
-  
 //Update Route
 app.put("/listings/:id", async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-  });
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
 
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+});
 
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
 
-//Adding a sample url for testing in database 
-// app.get("/testlisting", async(req,res)=>{
-//     let sampleListing=new Listing({
-//         title: "My Home",
-//         description: "This is my home page",
-//         price: 1000,
-//         location: "Kangra, Himachal Pradesh",
-//         country: "India"
-//     });
-//     await sampleListing.save();
-//     console.log("Sample was saved");
-//     res.send("<h1>Successfully created a new listing</h1>");
-
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
 // });
 
-app.listen(8000, ()=>{console.log('Server is running on port 8000');});
- 
+app.listen(8000, () => {
+  console.log("server is listening to port 8080");
+});
