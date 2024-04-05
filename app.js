ee323aed0981614f581e861b3a4652c67c6b5189
Gourav Chaudhary <gouravsittam@gmail.com>
2024-04-06 00:15:42 +0530

    up

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
+const methodOverride=require("method-override");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/IncredibleIndia";
 
@@ -21,7 +22,7 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-// app.use(methodOverride("_method")); 
+app.use(methodOverride("_method")); 
 
 
 
@@ -36,7 +37,12 @@ app.get("/listings", async (req, res) => {
   });
 
 
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+  });
  
+
   //Show Route
   app.get("/listings/:id", async (req, res) => {
     let { id } = req.params;
@@ -44,6 +50,27 @@ app.get("/listings", async (req, res) => {
     res.render("listings/show.ejs", { listing });
   });
 
+//Create Route
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  });
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  });
+
+  
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  });
 
 
 
