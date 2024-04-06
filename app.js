c40a738873bfcc5053ea26d4f7a63116938717c7
Gourav Chaudhary <gouravsittam@gmail.com>
2024-04-07 00:13:59 +0530

    add new files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-// const ejsMate = require("ejs-mate");
+const ejsMate = require("ejs-mate");//help for create template in websites
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/IncredibleIndia";
 
@@ -16,7 +16,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-// app.engine("ejs", ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 main()
